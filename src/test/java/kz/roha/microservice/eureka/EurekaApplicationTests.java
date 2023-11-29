1158f4c1243331e0eda687c32663516cfe67143e
RakhymKurmangali <rahym.kurmangali@mail.ru>
2023-11-29 11:02:59 +0600

    Init commit

---
+++ src/test/java/kz/roha/microservice/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.roha.microservice.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
